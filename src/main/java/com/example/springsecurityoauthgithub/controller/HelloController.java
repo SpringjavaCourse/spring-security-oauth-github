0ed16c00a41a79740151df38a2a2857aa0f3f407
duvan1026 <duvancastro1026@gmail.com>
2023-04-21 09:13:13 -0500

    implement OAuth with github

--- src/main/java/com/example/springsecurityoauthgithub/controller/HelloController.java
+++ src/main/java/com/example/springsecurityoauthgithub/controller/HelloController.java
@@ -1,2 +1,19 @@
-package com.example.springsecurityoauthgithub.controller;public class HelloController {
+package com.example.springsecurityoauthgithub.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // Controlador MVC
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/page1")
+    public String page1(){
+        return "page1";
+    }
 }
